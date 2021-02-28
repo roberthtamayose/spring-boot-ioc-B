444c97dc3ed2d996e5a993b497117cc93507a878
roberth tamayose <roberth.tamayose@gmail.com>
2021-02-28 14:45:16 -0300

    testando dominio Categoria

--- src/main/java/com/rtamayose/cursospringboot/resources/CategoriaResource.java
+++ src/main/java/com/rtamayose/cursospringboot/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.rtamayose.cursospringboot.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rtamayose.cursospringboot.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Request está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 		
 }
