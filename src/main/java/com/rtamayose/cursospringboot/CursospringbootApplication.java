cfcec8a71edbae8b96254fd1ccfdc4f61bfb9562
Roberth Tamayose <roberth.tamayose@gmail.com>
2022-08-31 22:56:54 -0300

    operação de instanciacao

--- src/main/java/com/rtamayose/cursospringboot/CursospringbootApplication.java
+++ src/main/java/com/rtamayose/cursospringboot/CursospringbootApplication.java
@@ -1,13 +1,32 @@
 package com.rtamayose.cursospringboot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursospringbootApplication {
+import com.rtamayose.cursospringboot.domain.Categoria;
+import com.rtamayose.cursospringboot.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursospringbootApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringbootApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception{
+		
+		Categoria cat1  = new Categoria(null, "Informática");
+		Categoria cat2  = new Categoria(null, "Escritorio");
+	
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
