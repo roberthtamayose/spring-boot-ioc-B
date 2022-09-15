5d5b3f7837ced3e47221a8ce9903967a95cfda4b
Roberth Tamayose <roberth.tamayose@gmail.com>
2022-09-15 17:08:02 -0300

    Categoria delete

--- src/main/java/com/rtamayose/cursospringboot/resources/CategoriaResource.java
+++ src/main/java/com/rtamayose/cursospringboot/resources/CategoriaResource.java
@@ -28,7 +28,7 @@ public class CategoriaResource {
 		
 	}
 	
-	@RequestMapping(method=RequestMethod.GET)
+	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
@@ -37,9 +37,18 @@ public class CategoriaResource {
 	}
 		
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
-	public ResponseEntity<Void> update (@RequestBody Categoria obj, @PathVariable Integer id){
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
 }
+
+
--- src/main/java/com/rtamayose/cursospringboot/services/CategoriaService.java
+++ src/main/java/com/rtamayose/cursospringboot/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.rtamayose.cursospringboot.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.rtamayose.cursospringboot.domain.Categoria;
 import com.rtamayose.cursospringboot.repositories.CategoriaRepository;
+import com.rtamayose.cursospringboot.services.exceptions.DataIntegrityException;
 import com.rtamayose.cursospringboot.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,4 +32,15 @@ public class CategoriaService {
 		find(obj.getId());
 		return repo.save(obj);
 	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+		}
+	}
+	
 }
