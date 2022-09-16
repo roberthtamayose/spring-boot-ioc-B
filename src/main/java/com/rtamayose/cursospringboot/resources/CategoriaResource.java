01e862b81783f4ece7e5d35bc5610b65cd011e92
Roberth Tamayose <roberth.tamayose@gmail.com>
2022-09-16 08:52:23 -0300

    Listando Categorias com DTO

--- src/main/java/com/rtamayose/cursospringboot/resources/CategoriaResource.java
+++ src/main/java/com/rtamayose/cursospringboot/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.rtamayose.cursospringboot.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rtamayose.cursospringboot.domain.Categoria;
+import com.rtamayose.cursospringboot.dto.CategoriaDTO;
 import com.rtamayose.cursospringboot.services.CategoriaService;
 
 @RestController
@@ -49,6 +52,15 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+		
+	}
+	
 }
 
 
--- src/main/java/com/rtamayose/cursospringboot/services/CategoriaService.java
+++ src/main/java/com/rtamayose/cursospringboot/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.rtamayose.cursospringboot.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,4 +44,7 @@ public class CategoriaService {
 		}
 	}
 	
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
 }
